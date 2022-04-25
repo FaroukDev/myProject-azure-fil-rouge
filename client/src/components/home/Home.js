7b543b7e3cbdb48fa6cd8ff867341e89ff36d5ff
FaroukDev <fa.elayache1@gmail.com>
2022-04-25 14:34:19 +0200

    testing endpoint authentification in azure client

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -11,7 +11,7 @@ function Home() {
   let history = useNavigate();
 
   useEffect(() => {
-    axios.get("https://authentification-app-back-farouk.azurewebsites.net/students").then((response) => {
+    axios.get("https://authentification-app-back-farouk.azurewebsites.net:80/students").then((response) => {
       setlistOfStudents(response.data);
     });
   }, []);

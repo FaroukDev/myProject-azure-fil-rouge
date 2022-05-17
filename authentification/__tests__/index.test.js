d9651214c79058efeaf3e84d8ff89d2fa15f6db0
FaroukDev <fa.elayache1@gmail.com>
2022-05-17 16:29:55 +0200

    setting url parameter in index.js in models directory

--- authentification/__tests__/index.test.js
+++ authentification/__tests__/index.test.js
@@ -1,9 +1,9 @@
 const request = require("supertest");
 const express = require("express");
 const router = require("../routes/Students.js");
-const config = require("../config/config.json");
+const models = require("../models");
 
-// afterAll(() => config.sequelize.close());
+afterAll(() => models.sequelize.close());
 
 const app = new express();
 

8f082338ea9e4221009bbe7e3cbca42d7c44d540
FaroukDev <fa.elayache1@gmail.com>
2022-05-17 15:06:13 +0200

    setup jest library to test endpoint

--- authentification/__tests__/index.test.js
+++ authentification/__tests__/index.test.js
@@ -1,35 +1,33 @@
-
-const request = require('supertest');
-const express = require('express');
-const router = require('../routes/Students.js');
-const models = require('../models');
-afterAll(() => models.sequelize.close())
-
+const request = require("supertest");
+const express = require("express");
+const router = require("../routes/Students.js");
+const models = require("../models");
+afterAll(() => models.sequelize.close());
 
 const app = new express();
 
-app.use('/', router);
+app.use("/", router);
 
-describe('list of students', function (){
-    test('responds to / ',async () => {
-        const res = await request(app).get('/');
-        expect(res.statusCode).toBe(200);
-        const expected = {
-            "id": 1,
-            "name": "farouk",
-            "description": "hello les devs :)",
-            "grade": "1st grade",
-            "createdAt": "2022-03-24T20:53:42.000Z",
-            "updatedAt": "2022-03-24T20:53:42.000Z"
-          }
-        const actual = {
-            "id": 1,
-            "name": "farouk",
-            "description": "hello les devs :)",
-            "grade": "1st grade",
-            "createdAt": "2022-03-24T20:53:42.000Z",
-            "updatedAt": "2022-03-24T20:53:42.000Z"
-          } 
-          expect(actual).toMatchObject(expected)
-    })
-})
+describe("list of students", function () {
+  test("responds to / ", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    const expected = {
+      id: 1,
+      name: "farouk",
+      description: "hello les devs :)",
+      grade: "1st grade",
+      createdAt: "2022-03-24T20:53:42.000Z",
+      updatedAt: "2022-03-24T20:53:42.000Z",
+    };
+    const actual = {
+      id: 1,
+      name: "farouk",
+      description: "hello les devs :)",
+      grade: "1st grade",
+      createdAt: "2022-03-24T20:53:42.000Z",
+      updatedAt: "2022-03-24T20:53:42.000Z",
+    };
+    expect(actual).toMatchObject(expected);
+  });
+});

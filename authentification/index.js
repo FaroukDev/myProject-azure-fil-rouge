a37a5d1b30907bee1d856ada7fe76a36d6d2548e
FaroukDev <fa.elayache1@gmail.com>
2022-05-17 16:46:41 +0200

    adding parameter in pipeline in azure devops

--- authentification/index.js
+++ authentification/index.js
@@ -8,8 +8,7 @@ const cors = require("cors");
 
 app.use(cors());
 
-
-const port = (process.env.PORT || 5001);
+const port = process.env.PORT || 5001;
 
 //Routers
 const postRouter = require("./routes/Students.js");

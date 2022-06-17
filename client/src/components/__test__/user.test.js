4503f53f2e883edad49b553db22a9c8e92502bdf
FaroukDev <fa.elayache1@gmail.com>
2022-06-17 15:53:46 +0200

    setting front test

--- client/src/components/__test__/user.test.js
+++ client/src/components/__test__/user.test.js
@@ -1,11 +1,18 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { MemoryRouter } from 'react-router-dom';
+import { act } from "react-dom/test-utils";
 import User from '../user/User.js'
 
 
-test('should render User component', () => {
+test('should render User component', async() => {
     render(<User/>, {wrapper: MemoryRouter});
     const UserElement = screen.getByTestId('User-1');
     expect(UserElement).toBeInTheDocument();
+
+    await act( async () => {
+        expect(UserElement).toBeInTheDocument();
+       });
+
+   
 })

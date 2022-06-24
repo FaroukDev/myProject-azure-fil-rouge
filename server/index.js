b5221f1137498ca5bec209006c9717500014eb12
FaroukDev <fa.elayache1@gmail.com>
2022-06-24 14:44:08 +0200

    setting connection string of mongo db

--- server/index.js
+++ server/index.js
@@ -1,20 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import studentRoutes from './routes/student.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import studentRoutes from "./routes/student.js";
 
 dotenv.config();
-
 const app = express();
-
 app.use(cors());
+app.use(express.json({ limit: "20mb", extented: true }));
+app.use(express.urlencoded({ limit: "20mb", extented: true }));
 
-app.use(express.json({limit: "20mb", extented:true}));
-app.use(express.urlencoded({limit: "20mb", extented:true}));
-
-app.use('/students', studentRoutes);
+app.use("/students", studentRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URI;
 
@@ -22,15 +18,18 @@ const PORT = process.env.PORT || 443;
 
 console.log(process.env.CONNECTION_URL);
 
-mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser:true, useUnifiedTopology:true
-}).then(() => app.listen(PORT, () => 
-    console.log(`Connection is etablished and running on port: ${PORT}`)
-)).catch((err) => console.log(err.message));
+//mongoose connection
 
-
-
-
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Connection is etablished and running on port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
 
 // mongoose.set('useFindAndModify', false);
-

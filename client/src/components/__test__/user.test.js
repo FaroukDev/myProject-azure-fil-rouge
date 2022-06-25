c24ff5859cb14dfd327e4d7a1ab978c3eec86aab
FaroukDev <fa.elayache1@gmail.com>
2022-06-25 16:00:12 +0200

    adding new test

--- client/src/components/__test__/user.test.js
+++ client/src/components/__test__/user.test.js
@@ -1,32 +1,28 @@
-import { render, screen, cleanup, getByTestId } from '@testing-library/react';
-import '@testing-library/jest-dom'
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen, cleanup, getByTestId } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
 import { act } from "react-dom/test-utils";
-import User from '../user/User.js';
-import {BrowserRouter as Router} from 'react-router-dom';
+import User from "../user/User.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
+// test component user
 
-test('should render User component',() => {
-    render(<User/>, {wrapper: MemoryRouter});
-    const UserElement = screen.getByTestId('User-1');
+test("should render User component", () => {
+  render(<User />, { wrapper: MemoryRouter });
+  const UserElement = screen.getByTestId("User-1");
+  expect(UserElement).toBeInTheDocument();
+
+  act(() => {
     expect(UserElement).toBeInTheDocument();
-    
-
-    act(() => {
-        expect(UserElement).toBeInTheDocument();
-        
-       });
-       
-   
-})
+  });
+});
 
 describe("User Component", () => {
-    it("rendered user",()=> {
-        <Router>
-           const {getByTestId} = render(<User/>);
-        const user = getByTestId("data-testid");
-        expect(user).toBeTruthy(); 
-        </Router>
-        
-    })
-})
+  it("rendered user", () => {
+    <Router>
+      const {getByTestId} = render(
+      <User />
+      ); const user = getByTestId("data-testid"); expect(user).toBeTruthy();
+    </Router>;
+  });
+});

38916ea36d531734a0cd3ecaea9905260db37288
FaroukDev <fa.elayache1@gmail.com>
2022-06-25 16:01:59 +0200

    adding new test

--- client/src/components/__test__/user.test.js
+++ client/src/components/__test__/user.test.js
@@ -5,7 +5,7 @@ import { act } from "react-dom/test-utils";
 import User from "../user/User.js";
 import { BrowserRouter as Router } from "react-router-dom";
 
-// test component user
+// test component 
 
 test("should render User component", () => {
   render(<User />, { wrapper: MemoryRouter });
